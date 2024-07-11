aa992b58b8ec0f08aff2e5d728225be1542ec184
ravi3575s <122154104+ravikla3575s@users.noreply.github.com>
2024-07-11 14:38:47 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,3 +1,9 @@
 let height = 1.7;
 let weight = 68;
-console.log (weight / (height ** 2));
+let bmi;
+bmi = weight / (height ** 2);
+console.log (bmi);
+// height =1.8
+// weight = 70
+// bmi = weight / (height ** 2);
+// console.log (bmi)
