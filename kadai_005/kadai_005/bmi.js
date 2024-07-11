1fd9152099f857c7c9d00bd1882eaff23fdc2566
ravi3575s <122154104+ravikla3575s@users.noreply.github.com>
2024-07-11 14:59:48 +0900

    Delete kadai_005/kadai_005 directory

--- kadai_005/kadai_005/bmi.js
+++
@@ -1,9 +0,0 @@
-let height = 1.7;
-let weight = 68;
-let bmi;
-bmi = weight / (height ** 2);
-console.log (bmi);
-// height =1.8
-// weight = 70
-// bmi = weight / (height ** 2);
-// console.log (bmi)
