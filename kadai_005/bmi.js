3578874cc5298710cc812ab9e0b0071a4aa2ded2
ravi3575s <122154104+ravikla3575s@users.noreply.github.com>
2024-07-11 14:43:07 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -3,7 +3,7 @@ let weight = 68;
 let bmi;
 bmi = weight / (height ** 2);
 console.log (bmi);
-// height =1.8
-// weight = 70
+// height =1.8；
+// weight = 70；
 // bmi = weight / (height ** 2);
 // console.log (bmi)
