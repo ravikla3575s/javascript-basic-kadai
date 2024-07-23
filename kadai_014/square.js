4be45130d89ba2cad30e6639fa98b4f08916ec27
ravikla3575s <imao3sw7.shgvoyeak4@gmail.com>
2024-07-23 21:55:55 +0900

    14章課題提出

---
+++ kadai_014/square.js
@@ -0,0 +1,11 @@
+// //アロー関数で定義 
+// const secondPower = (num) => {
+//     console.log(num ** 2)
+// }
+
+// Functionで定義
+function secondPower(num){
+    console.log(num ** 2)
+}
+// 引数100で戻り値を求める
+secondPower(100)
