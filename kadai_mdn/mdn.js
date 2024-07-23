e35665a1212c694d4b1c43a2b52c4a337bd2a077
ravikla3575s <imao3sw7.shgvoyeak4@gmail.com>
2024-07-23 21:21:01 +0900

    fix

--- kadai_mdn/mdn.js
+++ kadai_mdn/mdn.js
@@ -1,5 +1,12 @@
 // 日付を定義
-var traditionalTanabata = new Date('Aug 22,2023');
+var today = new Date();
 
-// console.logにて日付を形式に基づいて表示
-console.log(traditionalTanabata.getFullYear() + '年' + (traditionalTanabata.getMonth() + 1) + '月' + traditionalTanabata.getDate() + '日');
+// // console.logにて日付を形式に基づいて表示
+console.log(today.getFullYear() + '年' + (today.getMonth() + 1) + '月' + today.getDate() + '日');
+
+
+// // 日付を定義
+// var traditionalTanabata = new Date('Aug 22,2023');
+
+// // console.logにて日付を形式に基づいて表示
+// console.log(traditionalTanabata.getFullYear() + '年' + (traditionalTanabata.getMonth() + 1) + '月' + traditionalTanabata.getDate() + '日');
